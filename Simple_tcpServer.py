645f21596520747a675aba4d6758337c3951c6a1
DOUGLAS MARINHO DA SILVA <082190029@faculdade.cefsa.edu.br>
2024-02-22 21:42:01 -0300

    .

--- Simple_tcpServer.py
+++ Simple_tcpServer.py
@@ -1,6 +1,11 @@
 from socket import *
 
 serverPort = 15200
+g = 11
+n = 33
+y = 3
+r2 = g**y%n
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(("", serverPort))
 serverSocket.listen(5)  # The argument "listen" tells the socket library that we want to queue up to 5 connection requests (usually the maximum) before starting to refuse external connections. If the rest of the code is written correctly, this should be enough.
@@ -11,19 +16,30 @@ while True:  # Keep the server running indefinitely
         connectionSocket, addr = serverSocket.accept()
         print(f"Connection from {addr} established.")
 
+        isFirst = True
+        k = 0
+
         while True:  # Communicate with the client until the client closes the connection
             sentence = connectionSocket.recv(1024)
             if not sentence:  # If the client closes the connection, break out of the loop
                 break
-
+            
             received = sentence.decode("utf-8")
             print("Received From Client:", received)
 
-            capitalizedSentence = received.upper()  # Process the received data
+            if isFirst:
+                r1 = int(received)
+                k = r1**y%n
+                isFirst = False
 
-            connectionSocket.send(capitalizedSentence.encode("utf-8"))
+                connectionSocket.send(bytes(r2, "utf-8"))
+                print("Sent R2 back to Client:", capitalizedSentence)
+            else:
+                capitalizedSentence = received.upper()  # Process the received data
 
-            print("Sent back to Client:", capitalizedSentence)
+                connectionSocket.send(capitalizedSentence.encode("utf-8"))
+
+                print("Sent back to Client:", capitalizedSentence)
 
             if received.lower() == 'exit':
                 connectionSocket.close()
