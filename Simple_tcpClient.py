9fbc5ed9284327475b0be763436fc9db8a280859
PEDRO VINICIUS SIMOES CORADI <082190016@faculdade.cefsa.edu.br>
2024-02-22 21:41:29 -0300

    key

--- Simple_tcpClient.py
+++ Simple_tcpClient.py
@@ -1,20 +1,39 @@
 from socket import *
 
-g = 125
-n = 98
-x = 200
+g = 11
+n = 33
+r1 = 0
+r2 = 0
+k = 0
+keySent = False
 
 serverName = "10.1.70.32"
 serverPort = 15200
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 while True:
-    sentence = input("Input lowercase sentence: ")
-    if not sentence == "":
-        clientSocket.send(bytes(sentence, "utf-8"))
-        modifiedSentence = clientSocket.recv(1024)
-        text = str(modifiedSentence,"utf-8")
-        print ("Received from Make Upper Case Server: ", text)
-        if text == "EXIT":
-            clientSocket.detach()
-            break
+
+    if not keySent:
+        x = input("Define a personal key for this connection: ")
+        r1 = (g**x)%n
+        keySent = True
+        clientSocket.send(bytes(r1, "utf-8"))
+        print("Waiting for server key...")
+        r2 = clientSocket.recv(1024)
+        k = (r2**x)%n
+    else:
+        sentence = input("Input lowercase sentence: ")
+        if not sentence == "":
+            clientSocket.send(bytes(sentence, "utf-8"))
+            print("Waiting for answer...")
+            modifiedSentence = clientSocket.recv(1024)
+            rawText = str(modifiedSentence,"utf-8")
+            for c in rawText:
+                c = ord(c) + k
+            print ("Received from \"Make Upper Case\" Server: ", rawText)
+            if rawText == "EXIT":
+                clientSocket.detach()
+                r2 = 0
+                k = 0
+                keySent = False
+                break
